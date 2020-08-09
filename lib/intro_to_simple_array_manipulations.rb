9f064feb93d5d06650acae1c9ff05414ef293cbe
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-09 21:58:12 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_push(a,s)
+   a.push(s)
+end 
+ 
+def using_unshift(a,s)
+  a.unshift(s)
+   
+end
+ 
+def using_pop(a)
+   return a.pop
+end
+ 
+def pop_with_args(a)
+   return a.pop(2)
+end
+
+def using_shift(a)
+   return a.shift
+end
+
+def shift_with_args(a)
+   return a.shift(2)
+end
